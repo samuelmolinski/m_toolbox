68d2854c4bbedda1206338d5463fad8cb73dfe17
Samuel Molinski <samuel.molinski@creativefocus.com.br>
2012-10-08 15:50:18 -0300

    -added functions to get the redirected urls from a link

--- inc/generic/m_uri.php
+++ inc/generic/m_uri.php
@@ -157,13 +157,77 @@
 		return $res;
 	}
 
-	// This lists projects by Ed Finkler on GitHub:
-	// foreach (rest_helper('http://github.com/api/v2/json/repos/show/funkatron')
-	// ->repositories as $repo) {
-		// echo $repo -> name, "<br>\n";
-		// echo htmlentities($repo -> description), "<br>\n";
-		// echo "<hr>\n";
-	// }
-
-	// This incomplete snippet demonstrates using POST with the Disqus API
-	///var_dump(rest_helper("http://disqus.com/api/thread_by_identifier/", array('api_version' => '1.1', 'user_api_key' => $my_disqus_api_key, 'identifier' => $thread_unique_id, 'forum_api_key' => $forum_api_key, 'title' => 'HTTP POST from PHP, without cURL', ), 'POST'));
+	/**
+	 * get_redirect_url()
+	 * Gets the address that the provided URL redirects to,
+	 * or FALSE if there's no redirect. 
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	function get_redirect_url($url){
+	    $redirect_url = null; 
+	 
+	    $url_parts = @parse_url($url);
+	    if (!$url_parts) return false;
+	    if (!isset($url_parts['host'])) return false; //can't process relative URLs
+	    if (!isset($url_parts['path'])) $url_parts['path'] = '/';
+	      
+	    $sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80), $errno, $errstr, 30);
+	    if (!$sock) return false;
+	      
+	    $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n"; 
+	    $request .= 'Host: ' . $url_parts['host'] . "\r\n"; 
+	    $request .= "Connection: Close\r\n\r\n"; 
+	    fwrite($sock, $request);
+	    $response = '';
+	    while(!feof($sock)) $response .= fread($sock, 8192);
+	    fclose($sock);
+	 
+	    if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
+	        if ( substr($matches[1], 0, 1) == "/" )
+	            return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
+	        else
+	            return trim($matches[1]);
+	  
+	    } else {
+	        return false;
+	    }
+	     
+	}
+	 
+	/**
+	 * get_all_redirects()
+	 * Follows and collects all redirects, in order, for the given URL. 
+	 *
+	 * @param string $url
+	 * @return array
+	 */
+	function get_all_redirects($url){
+	    $redirects = array();
+	    while ($newurl = get_redirect_url($url)){
+	        if (in_array($newurl, $redirects)){
+	            break;
+	        }
+	        $redirects[] = $newurl;
+	        $url = $newurl;
+	    }
+	    return $redirects;
+	}
+	 
+	/**
+	 * get_final_url()
+	 * Gets the address that the URL ultimately leads to. 
+	 * Returns $url itself if it isn't a redirect.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	function get_final_url($url){
+	    $redirects = get_all_redirects($url);
+	    if (count($redirects)>0){
+	        return array_pop($redirects);
+	    } else {
+	        return $url;
+	    }
+	}
